f1136c69ef21c5b691ba98601b85e4c1aea902d6
gaius-qi <gaius.qi@gmail.com>
2021-02-01 17:40:22 +0800

    feat: channel

---
+++ channel/range/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func makeCakeAndSend(cs chan string, count int) {
+	for i := 1; i <= count; i++ {
+		cakeName := "Strawberry Cake " + strconv.Itoa(i)
+		cs <- cakeName //send a strawberry cake
+	}
+	close(cs)
+}
+
+func receiveCakeAndPack(cs chan string) {
+	for s := range cs {
+		fmt.Println("Packing received cake: ", s)
+	}
+}
+
+func main() {
+	cs := make(chan string)
+	go makeCakeAndSend(cs, 5)
+	receiveCakeAndPack(cs)
+}
