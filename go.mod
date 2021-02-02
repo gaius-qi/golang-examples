dd6c6d40170233712b89df3dd0fbb44a2def1b47
gaius-qi <gaius.qi@gmail.com>
2021-02-02 11:36:39 +0800

    feat: load config

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
+	github.com/dragonflyoss/Dragonfly v1.0.6
 	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2
 )
---
+++ yaml/load/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	config := NewConfig()
+	err := config.Load("./config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(config, config.Name, config.Nodes[0].ID)
+}
+
+const (
+	defaultName = "default"
+)
+
+type Config struct {
+	Name  string    `yaml:"name,omitempty" json:"name,omitempty"`
+	Nodes []*Weight `yaml:"nodes,omitempty" json:"nodes,omitempty"`
+}
+
+type Weight struct {
+	ID     string
+	Weight int
+}
+
+func NewConfig() *Config {
+	// don't set Supernodes as default value, the SupernodeLocator will
+	// do this in a better way.
+	return &Config{
+		Name: defaultName,
+	}
+}
+
+// Load loads properties from config file.
+func (c *Config) Load(path string) error {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to load yaml %s when reading file: %v", path, err)
+	}
+	if err = yaml.Unmarshal(content, c); err != nil {
+		return fmt.Errorf("failed to load yaml %s: %v", path, err)
+	}
+	return nil
+}
