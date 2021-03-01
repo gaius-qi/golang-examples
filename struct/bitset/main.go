b16840372fb848c4c28737de16a44318aa803294
gaius-qi <gaius.qi@gmail.com>
2021-03-01 16:16:40 +0800

    feat: bitset

---
+++ struct/bitset/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/willf/bitset"
+)
+
+func main() {
+	b := bitset.New(2)
+	fmt.Printf("empty bitset: %v\n", b.All())
+
+	b.Set(0)
+	fmt.Printf("0 bitset: %v\n", b.All())
+
+	b.Set(0).Set(1)
+	fmt.Printf("0 1 bitset: %v\n", b.All())
+}
