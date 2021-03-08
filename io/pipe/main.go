eba3221ab1dab32b7c8eb0791cbeedcd186d7c6a
gaius-qi <gaius.qi@gmail.com>
2021-03-08 17:09:03 +0800

    feat: io.pipe

---
+++ io/pipe/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+func main() {
+	r, w := io.Pipe()
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			w.Write([]byte(time.Now().String()))
+		}
+	}()
+	for {
+		dataRead := make([]byte, 256)
+		n, _ := r.Read(dataRead)
+		fmt.Println(string(dataRead[:n]))
+	}
+}
