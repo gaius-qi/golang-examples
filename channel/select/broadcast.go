ae21dc4914c8a36fe547919e1c20076b119dcd0e
gaius-qi <gaius.qi@gmail.com>
2021-03-08 20:10:11 +0800

    feat: broadcast

---
+++ channel/select/broadcast.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	N := 10
+	exit := make(chan struct{})
+	done := make(chan struct{}, N)
+
+	// start N worker goroutines
+	for i := 0; i < N; i++ {
+		go func(n int) {
+			for {
+				select {
+				// wait for exit signal
+				case <-exit:
+					fmt.Printf("worker goroutine #%d exit\n", n)
+					done <- struct{}{}
+					return
+				case <-time.After(time.Second):
+					fmt.Printf("worker goroutine #%d is working...\n", n)
+				}
+			}
+		}(i)
+	}
+
+	time.Sleep(3 * time.Second)
+	// broadcast exit signal
+	close(exit)
+	// wait for all worker goroutines exit
+	for i := 0; i < N; i++ {
+		<-done
+	}
+	fmt.Println("main goroutine exit")
+}
