900a339f3109f177253d317683afb715d6092fdc
Gaius <gaius.qi@gmail.com>
2021-03-26 11:18:10 +0800

    feat: go-cache

--- cache/go-cache/main.go
+++ cache/go-cache/main.go
@@ -20,15 +20,22 @@ func main() {
 
 	// Init cat
 	cat := &Animal{
-		name:   "cat",
+		name:   "jack",
 		number: 1,
 		height: 1,
 	}
 
 	// Set the value of the key "foo" to "bar", with the default expiration time
-	c.Set("cat", cat, cache.DefaultExpiration)
+	c.Set("cat", cat, 3*time.Second)
 	if x, found := c.Get("cat"); found {
 		cat := x.(*Animal)
-		fmt.Printf("name: %s, number: %d, height: %d", cat.name, cat.number, cat.height)
+		fmt.Printf("name: %s, number: %d, height: %d\n", cat.name, cat.number, cat.height)
 	}
+
+	time.Sleep(2 * time.Second)
+	x, duration, ok := c.GetWithExpiration("cat")
+	fmt.Printf("expire: %t, duration: %s, value: %#v\n", ok, duration, x)
+
+	x, found := c.Get("cat")
+	fmt.Printf("found: %t, value: %#v\n", found, x)
 }
