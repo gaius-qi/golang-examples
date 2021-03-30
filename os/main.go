6e7997e6e4c68120fc0f275b0edc20c227017413
Gaius <gaius.qi@gmail.com>
2021-03-30 16:13:58 +0800

    feat: user cache dir

--- os/main.go
+++ os/main.go
@@ -6,11 +6,19 @@ import (
 )
 
 func main() {
-	f, err := os.Create("./user/cache")
+	f, err := os.Create("./test/cache")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Printf("create file success: %v", f)
+
+	dir, err := os.UserCacheDir()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("user cache dir: %v", dir)
 }
