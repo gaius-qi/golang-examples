5e1b315ec3de97b65ab29b92e475a79aabf049ea
Gaius <gaius.qi@gmail.com>
2021-03-31 18:03:07 +0800

    feat: gomock

--- go.mod
+++ go.mod
@@ -1,13 +1,14 @@
-module golang-examples
+module github.com/gaius-qi/golang-examples
 
 go 1.15
 
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e
+	github.com/golang/mock v1.5.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/sgreben/testing-with-gomock v0.0.0-20180127205614-2990fb8af60c
 	github.com/spf13/viper v1.7.1
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/willf/bitset v1.1.11
-	gopkg.in/yaml.v2 v2.2.4
+	gopkg.in/yaml.v2 v2.4.0
 )
---
+++ gomock/sample/user/user_test.go
@@ -0,0 +1,40 @@
+package user
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/gaius-qi/golang-examples/gomock/sample/mocks"
+	"github.com/golang/mock/gomock"
+)
+
+func TestUse(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockDoer := mocks.NewMockDoer(mockCtrl)
+	testUser := &User{Doer: mockDoer}
+
+	// Expect Do to be called once with 123 and "Hello GoMock" as parameters, and return nil from the mocked call.
+	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(nil).Times(1)
+
+	testUser.Use()
+}
+
+func TestUseReturnsErrorFromDo(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	dummyError := errors.New("dummy error")
+	mockDoer := mocks.NewMockDoer(mockCtrl)
+	testUser := &User{Doer: mockDoer}
+
+	// Expect Do to be called once with 123 and "Hello GoMock" as parameters, and return dummyError from the mocked call.
+	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(dummyError).Times(1)
+
+	err := testUser.Use()
+
+	if err != dummyError {
+		t.Fail()
+	}
+}
