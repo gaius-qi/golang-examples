77dc876221cb5561dec3dabe87dd739af9c73f9b
Gaius <gaius.qi@gmail.com>
2021-06-10 10:32:01 +0800

    feat: ioutil
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ ioutil/read-dir-files/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type Book struct {
+	Id    string `json: "id"`
+	Title string `json: "title"`
+}
+
+func main() {
+	fileInfos, err := ioutil.ReadDir("./testdata")
+	if err != nil {
+		fmt.Println("Error in accessing directory:", err)
+	}
+
+	var books []Book
+	for _, file := range fileInfos {
+		b, err := ioutil.ReadFile(filepath.Join("./testdata", file.Name()))
+		if err != nil {
+			panic(err)
+		}
+
+		var book Book
+		if err := json.Unmarshal(b, &book); err != nil {
+			panic(err)
+		}
+
+		books = append(books, book)
+		fmt.Println(books)
+	}
+}
