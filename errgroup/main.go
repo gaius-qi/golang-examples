6fdd23ff728c68935d28919dcd435eb34effe63c
Gaius <gaius.qi@gmail.com>
2021-08-18 16:08:42 +0800

    feat: context
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ errgroup/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	g := errgroup.Group{}
+	g.Go(func() error {
+		return errors.New("goroutine 1")
+	})
+
+	g.Go(func() error {
+		time.Sleep(3 * time.Second)
+		return errors.New("goroutine 2")
+	})
+
+	werr := g.Wait()
+	fmt.Println(werr)
+
+	g2, ctx := errgroup.WithContext(context.Background())
+	g2.Go(func() error {
+		defer ctx.Done()
+		return errors.New("goroutine 1")
+	})
+
+	g2.Go(func() error {
+		defer ctx.Done()
+		time.Sleep(3 * time.Second)
+		return errors.New("goroutine 2")
+	})
+
+	werr2 := g.Wait()
+	fmt.Println(werr2)
+}
