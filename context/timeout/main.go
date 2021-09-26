614a76491f8a7b4978b8e3ebafb637cfcf1febc4
Gaius <gaius.qi@gmail.com>
2021-09-26 12:25:27 +0800

    feat: context
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- context/timeout/main.go
+++ context/timeout/main.go
@@ -13,6 +13,7 @@ func main() {
 func asyncCall() {
 	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
 	defer cancel()
+
 	go func(ctx context.Context) {
 		time.Sleep(3 * time.Second)
 		fmt.Println("reuqest!!!")
