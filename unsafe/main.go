6c91c66e3c1dbbb9fedd708a98e66257a7abc7c1
Gaius <gaius.qi@gmail.com>
2021-11-12 14:15:50 +0800

    feat: size
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- unsafe/main.go
+++ unsafe/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
@@ -15,10 +17,31 @@ func main() {
 	fmt.Println(unsafe.Sizeof(m))
 	fmt.Println(unsafe.Sizeof(s))
 	fmt.Println(unsafe.Sizeof(h))
+	fmt.Println(unsafe.Sizeof([]map[string]struct{}{}))
+
+	d := []map[string]struct{}{}
+	for i := 0; i < 1000000; i++ {
+		d = append(d, map[string]struct{}{})
+	}
+
 	var rtm runtime.MemStats
 	runtime.ReadMemStats(&rtm)
 	fmt.Printf("%.2f MB", float64(rtm.Alloc)/1024./1024.)
 	fmt.Printf("%.2f MB", float64(rtm.HeapAlloc)/1024./1024.)
+	fmt.Printf("%.2f MB", float64(rtm.Mallocs)/1024./1024.)
+	fmt.Printf("%.2f MB", float64(rtm.MSpanInuse)/1024./1024.)
+
+	_ = m
+
+	fmt.Println("---------------")
+
+	b := new(bytes.Buffer)
+	gob.NewEncoder(b).Encode(m)
+	fmt.Println("11111", b.Len())
+
+	e := new(bytes.Buffer)
+	gob.NewEncoder(e).Encode(d)
+	fmt.Println("11111", e.Len())
 
 	go func() {
 		http.ListenAndServe("0.0.0.0:9999", nil)
