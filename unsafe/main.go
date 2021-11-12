a431256a96b0e624f677cd29e181a067e227aec9
Gaius <gaius.qi@gmail.com>
2021-11-12 11:52:05 +0800

    feat: slice
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- unsafe/main.go
+++ unsafe/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	_ "net/http/pprof"
 	"runtime"
 	"unsafe"
 )
 
 func main() {
-	m := make(map[string]struct{}, 1000000)
-	s := make(map[string]struct{}, 1000000)
-	h := make(map[string]struct{}, 1000000)
+	m := make([]map[string]struct{}, 1000000)
+	s := make([]map[string]struct{}, 1000000)
+	h := make([]map[string]struct{}, 1000000)
 	fmt.Println(unsafe.Sizeof(m))
 	fmt.Println(unsafe.Sizeof(s))
 	fmt.Println(unsafe.Sizeof(h))
 	var rtm runtime.MemStats
 	runtime.ReadMemStats(&rtm)
 	fmt.Printf("%.2f MB", float64(rtm.Alloc)/1024./1024.)
+	fmt.Printf("%.2f MB", float64(rtm.HeapAlloc)/1024./1024.)
+
+	go func() {
+		http.ListenAndServe("0.0.0.0:9999", nil)
+	}()
+
+	select {}
 }
