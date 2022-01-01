626fd04e9242e1f5cf0a47ad182e622648b844bb
Gaius <gaius.qi@gmail.com>
2022-01-01 13:47:13 +0800

    feat: exec
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ exec/command/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	out, err := exec.Command("mv", "foo.log", "logs/foo.log").CombinedOutput()
+	if err != nil {
+		fmt.Println("move failed: ", err)
+	}
+	fmt.Println(out)
+}
