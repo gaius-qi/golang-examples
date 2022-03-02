17fb971342afc5a054f75e891df1a9dd978e86fb
Gaius <gaius.qi@gmail.com>
2022-03-02 21:17:05 +0800

    feat: time
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- time/main.go
+++ time/main.go
@@ -6,5 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Printf("%d", 60*time.Second)
+	fmt.Println(60 * time.Second)
+
+	t1 := time.Now().UnixNano()
+	at1 := time.Unix(0, t1)
+	time.Sleep(2 * time.Second)
+	t2 := time.Now().UnixNano()
+	at2 := time.Unix(0, t2)
+	aa := at2.Sub(at1)
+	fmt.Println(aa.Nanoseconds())
 }
