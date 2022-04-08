d26209827610865568e6306f9b81d0e826de0c4f
Gaius <gaius.qi@gmail.com>
2022-04-08 12:07:34 +0800

    feat: gota
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- gota/main.go
+++ gota/main.go
@@ -7,12 +7,21 @@ import (
 	"github.com/go-gota/gota/dataframe"
 )
 
+type User struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	f, err := os.Open("./data.csv")
 	if err != nil {
 		panic(err)
 	}
 
-	df := dataframe.ReadCSV(f)
-	fmt.Println(df)
+	df1 := dataframe.ReadCSV(f)
+	fmt.Println(df1)
+
+	users := []User{{Name: "jack", Age: 18}, {Name: "lucy", Age: 20}}
+	df2 := dataframe.LoadStructs(users)
+	fmt.Println(df2)
 }
