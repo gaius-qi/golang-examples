9890123dade63dfc020e00d3bdb750cc194d1f5a
Gaius <gaius.qi@gmail.com>
2022-04-13 14:51:28 +0800

    feat: slice
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- len/main.go
+++ len/main.go
@@ -14,6 +14,21 @@ func main() {
 	ns1 := make([]*User, 2)
 	fmt.Println(len(ns1))
 
-	ns2 := make([]*User, 0, 1)
+	ns2 := make([]*User, 0, 3)
 	fmt.Println(len(ns2))
+
+	ns2 = append(ns2, &User{Name: "jack"}, &User{Name: "lucy"})
+	fmt.Println(ns2, ns2[0].Name, ns2[1].Name, len(ns2), cap(ns2))
+
+	ns2 = ns2[:0]
+	fmt.Println(ns2, len(ns2), cap(ns2))
+
+	ns2 = append(ns2, &User{Name: "gaius"})
+	fmt.Println(ns2, ns2[0].Name, len(ns2), cap(ns2))
+
+	ns2 = append(ns2, &User{Name: "gaius"})
+	ns2 = append(ns2, &User{Name: "gaius"})
+	ns2 = append(ns2, &User{Name: "gaius"})
+	ns2 = append(ns2, &User{Name: "gaius"})
+	fmt.Println(ns2, len(ns2), cap(ns2))
 }
