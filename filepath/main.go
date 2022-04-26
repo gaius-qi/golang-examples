1f5d7aa22f7dfd79c265dc3c0ab2b9ce9128b6fa
Gaius <gaius.qi@gmail.com>
2022-04-26 13:31:54 +0800

    feat: filepath
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- filepath/main.go
+++ filepath/main.go
@@ -9,4 +9,5 @@ func main() {
 	fmt.Println(filepath.Base("/foo/bar/baz.js"))
 	fmt.Println(filepath.Dir("/foo/bar/baz.js"))
 	fmt.Println(filepath.Join("/foo", "bar/baz.js"))
+	fmt.Println(filepath.Join("/foo", "/bar/baz.js"))
 }
