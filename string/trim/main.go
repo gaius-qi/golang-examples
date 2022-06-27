beecaae5d3e0eee27afa8873cac4e2ce0b67e05a
Gaius <gaius.qi@gmail.com>
2022-06-27 14:57:09 +0800

    feat: trim
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- string/trim/main.go
+++ string/trim/main.go
@@ -8,4 +8,16 @@ import (
 func main() {
 	s := "aaa/cc/vv"
 	fmt.Println(strings.TrimRight(s, "/"))
+
+	d := "/aaa/cc/vv"
+	fmt.Println(strings.TrimLeft(d, "/"))
+
+	d = "/aaa/cc/vv"
+	fmt.Println(strings.TrimLeft(d, "/"))
+
+	e := "/aaa/cc/vv"
+	fmt.Println(strings.TrimPrefix(e, "/"))
+
+	e = "aaa/cc/vv"
+	fmt.Println(strings.TrimPrefix(e, "/"))
 }
