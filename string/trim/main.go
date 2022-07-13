c3638560bd08c9706a4baa95ad0a5a148c94be6e
Gaius <gaius.qi@gmail.com>
2022-07-13 17:04:09 +0800

    feat: ipv4
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- string/trim/main.go
+++ string/trim/main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
 	s := "aaa/cc/vv"
-	fmt.Println(strings.TrimRight(s, "/"))
+	fmt.Println(strings.TrimRight(s, string(os.PathSeparator)))
 
 	d := "/aaa/cc/vv"
-	fmt.Println(strings.TrimLeft(d, "/"))
+	fmt.Println(strings.TrimLeft(d, string(os.PathSeparator)))
 
 	d = "/aaa/cc/vv"
-	fmt.Println(strings.TrimLeft(d, "/"))
+	fmt.Println(strings.TrimLeft(d, string(os.PathSeparator)))
 
 	e := "/aaa/cc/vv"
-	fmt.Println(strings.TrimPrefix(e, "/"))
+	fmt.Println(strings.TrimPrefix(e, string(os.PathSeparator)))
 
 	e = "aaa/cc/vv"
-	fmt.Println(strings.TrimPrefix(e, "/"))
+	fmt.Println(strings.TrimPrefix(e, string(os.PathSeparator)))
+
+	fmt.Println(filepath.Join("/", "/cdas/b"))
+	fmt.Println(filepath.Join("/", "cdas/b"))
 }
