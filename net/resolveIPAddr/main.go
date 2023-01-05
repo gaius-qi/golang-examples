48c66fc9e6c67d8d1bd20372612cb2a1555f4291
Gaius <gaius.qi@gmail.com>
2023-01-05 12:52:01 +0800

    feat: resolve
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- net/resolveIPAddr/main.go
+++ net/resolveIPAddr/main.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-	ra, err := net.ResolveIPAddr("ip6:icmp", "2402:4e00::b")
+	r1, err := net.ResolveIPAddr("ip4:icmp", "39.156.66.10")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("ipv4: %s\n", r1.String())
 
-	fmt.Printf("ip: %s\n", ra.String())
+	r2, err := net.ResolveIPAddr("ip6:icmp", "2402:4e00::b")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("ipv6: %s\n", r2.String())
 }
