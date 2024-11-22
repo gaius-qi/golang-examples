2cbff9b126ad2619d5c3d4246a5f0a765363dab6
Gaius <gaius.qi@gmail.com>
2024-11-22 18:27:31 +0800

    feat: update
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- proxy/http/main.go
+++ proxy/http/main.go
@@ -1,47 +1,67 @@
 package main
 
 import (
-	"crypto/tls"
-	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
-	"syscall"
+	"sync"
 	"time"
 )
 
-var httpClient = &http.Client{
-	Transport: &http.Transport{
-		Proxy:                 http.ProxyFromEnvironment,
-		TLSHandshakeTimeout:   time.Second * 20,
-		ResponseHeaderTimeout: time.Second * 30,
-		IdleConnTimeout:       time.Second * 300,
-		MaxIdleConnsPerHost:   500,
-		ReadBufferSize:        32 << 10,
-		WriteBufferSize:       32 << 10,
-		DisableCompression:    true,
-		TLSClientConfig:       &tls.Config{},
-	},
-	Timeout: time.Hour,
+func main() {
+	var wg sync.WaitGroup
+	n := 50
+	target := "http://127.0.0.1:8081/medium"
+	proxy := "http://127.0.0.1:4001"
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func(target string) {
+			defer wg.Done()
+			err := download(target, proxy)
+			if err != nil {
+				fmt.Printf("Failed to download %s: %v\n", target, err)
+			}
+		}(target)
+	}
+
+	wg.Wait()
+	fmt.Println("All downloads are done")
 }
 
-func main() {
-	proxyURL, _ := url.Parse("http://127.0.0.1:65001")
-	httpClient.Transport.(*http.Transport).Proxy = http.ProxyURL(proxyURL)
+func download(target string, proxy string) error {
+	proxyURL, err := url.Parse(proxy)
+	if err != nil {
+		return err
+	}
 
-	req, _ := http.NewRequest("GET", "https://www.google.com", nil)
+	req, err := http.NewRequest("GET", target, nil)
+	if err != nil {
+		return err
+	}
+
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+		Timeout: time.Hour,
+	}
 
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		if errors.Is(err, syscall.ECONNREFUSED) {
-			fmt.Println("Connection refused")
-		}
-		panic(err)
+		return err
 	}
+	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		panic(resp.Status)
+		return fmt.Errorf("status code: %d", resp.StatusCode)
 	}
 
-	fmt.Println(resp.Status)
+	if _, err := io.ReadAll(resp.Body); err != nil {
+		fmt.Printf("Failed to read response body: %v\n", err)
+		return err
+	}
+
+	return nil
 }
