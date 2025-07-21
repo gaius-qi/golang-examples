86863cbc3aa257e0e8252b949920ed052c5a88b7
Gaius <gaius.qi@gmail.com>
2025-07-21 19:59:05 +0800

    feat: layers
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ layers/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type Node struct {
+	IP                 string  `json:"ip,omitempty"`
+	Hostname           string  `json:"hostname,omitempty"`
+	Layers             []Layer `json:"layers"`
+	SchedulerClusterID int     `json:"scheduler_cluster_id"`
+}
+
+type Layer struct {
+	URL string `json:"url"`
+}
+
+type Image struct {
+	Nodes []Node `json:"peers"`
+}
+
+func main() {
+	// Read the JSON file
+	data, err := ioutil.ReadFile("jsonformatter.txt")
+	if err != nil {
+		log.Fatalf("Error reading JSON file: %v", err)
+	}
+
+	// Parse JSON data
+	var image Image
+	err = json.Unmarshal(data, &image)
+	if err != nil {
+		log.Fatalf("Error parsing JSON: %v", err)
+	}
+
+	// Count layers for each node
+	for i, node := range image.Nodes {
+		var identifier string
+		if node.IP != "" && node.Hostname != "" {
+			identifier = fmt.Sprintf("Node %d (IP: %s, Hostname: %s)", i+1, node.IP, node.Hostname)
+		} else {
+			identifier = fmt.Sprintf("Node %d", i+1)
+		}
+		fmt.Printf("%s has %d layers\n", identifier, len(node.Layers))
+	}
+}
